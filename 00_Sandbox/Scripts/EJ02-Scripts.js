9d28d00225923ea1bae3050506c3361a32475fca
Tepa <116313310+TepaCode@users.noreply.github.com>
2023-05-26 20:10:51 -0600

    Upload script EJ02
    Script ejercicio 02

---
+++ 00_Sandbox/Scripts/EJ02-Scripts.js
@@ -0,0 +1,10 @@
+//---------Buttons functions-------
+//import random
+function boxRandom(){
+   // const premios= ["pizza", "hamburguesas", "vino", "viaje"];
+    //const premiosR=premios.random()
+    alert("Haz ganado pizza gratis!!!!")
+}
+function greetings(){
+    alert("Hola " + document.getElementById("input-one").value)
+}
